ebb381502d5ac93e3609dca2b4b842d5a31781cb
Paul Kögel <paul@railslove.com>
2016-04-01 15:28:44 +0200

    Add Reagent.

--- src/devcards_playground/core.cljs
+++ src/devcards_playground/core.cljs
@@ -1,16 +1,40 @@
 (ns devcards-playground.core
   (:require
-   #_[om.core :as om :include-macros true]
-   [sablono.core :as sab :include-macros true])
+    [devcards.core]
+    [reagent.core :as reagent])
   (:require-macros
-   [devcards.core :as dc :refer [defcard deftest]]))
+    [devcards.core :as dc :refer [defcard defcard-rg defcard-doc]]))
 
-(enable-console-print!)
+; app state reagent atom (keeps track of where it is dereferenced so those places receive updates upon change)
+(def click-count (reagent/atom 0))
 
-(defcard first-card
-  (sab/html [:div
-             [:h1 "This is your first devcard!"]]))
+; simple Reagent counter component
+(defn counting-component []
+  [:div
+   "The atom " [:code "click-count"] " has value: "
+   @click-count ". "
+   [:button {:on-click #(swap! click-count inc)} "Click meaou!"]])
 
-;; remember to run lein figwheel and then browse to
-;; http://localhost:3449/cards.html
+(defcard reagent-counting-component
+         (dc/reagent counting-component)
+         click-count
+         {:inspect-data true :history true })
 
+
+(defn __ [s1 s2]
+  (clojure.set/union
+    (clojure.set/difference s1 s2)
+    (clojure.set/difference s2 s1)
+  ))
+
+(js/console.log
+  (= (__ #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7})
+  (= (__ #{:a :b :c} #{}) #{:a :b :c})
+  (= (__ #{} #{4 5 6}) #{4 5 6})
+  (= (__ #{[1 2] [2 3]} #{[2 3] [3 4]}) #{[1 2] [3 4]})
+  )
+
+(defcard-doc
+  "some documentation on `__`"
+  (dc/mkdn-pprint-source __)
+  "that was it!")
